7656deca24a455f23296f42023c190115d3c57c2
PhatSut <b6202385@g.sut.ac.th>
2022-01-16 15:18:17 +0700

    close #70 -setup backend and frontend

---
+++ backend/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sut64/team07/controller"
+	"github.com/sut64/team07/entity"
+)
+
+func main() {
+	entity.SetupDatabase()
+	r := gin.Default()
+	r.Use(CORSMiddleware())
+	
+	// Run the server
+	r.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+
+}
