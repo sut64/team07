e6e38c0d3361f71007fb32103992e68d228125cf
B6202385 <phatcha36.1.2.56@gmail.com>
2022-01-23 01:08:36 +0700

    close #17 - Entity ExamSchedule and Setup

--- backend/entity/schema.go
+++ backend/entity/schema.go
@@ -1,17 +1,35 @@
 package entity
 import (
-	//  "time"
+	  "time"
 	  "gorm.io/gorm"
 	)
 	 
 	type Semester struct {
 		gorm.Model
 		Semester  string
-		//ExamSchedule []ExamSchedule `gorm:"foreignKey:SemesterID"`
+		ExamSchedule []ExamSchedule `gorm:"foreignKey:SemesterID"`
 	}
 
 	type ExamType struct {
 		gorm.Model
 		Type  string
-		//ExamSchedule []ExamSchedule `gorm:"foreignKey:ExamTypeID"`
+		ExamSchedule []ExamSchedule `gorm:"foreignKey:ExamTypeID"`
+	}
+
+	type ExamSchedule struct {
+		gorm.Model
+		AcamedicYear int16
+		RoomExam     string
+		DateExam	 time.Time
+		StartTime 	 time.Time
+		EndTime		 time.Time
+	
+		/*CourseID *uint
+		Course   Course `gorm:"references:id"`*/
+	
+		ExamTypeID *uint
+		ExamType   ExamType `gorm:"references:id"`
+	
+		SemesterID *uint
+		Semester   Semester `gorm:"references:id"`
 	}
