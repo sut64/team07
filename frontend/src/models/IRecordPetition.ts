55e030cbd8ac21de5d6402d2eaccde977738336f
Not09 <73130087+Not09@users.noreply.github.com>
2022-01-27 21:40:02 +0700

    close #50 -Add RecordPetitionUI

---
+++ frontend/src/models/IRecordPetition.ts
@@ -0,0 +1,23 @@
+import { CoursesInterface } from "./ICourse";
+import { PetitionsInterface } from "./IPetition";
+import { StudentsInterface } from "./IStudent";
+
+
+export interface RecordPetitionInterface {
+    
+    ID: number,
+
+	Because:            string,
+	RegisteredCredit:   number,
+	TimeRecord:         Date,
+    
+    StudentID:          number,
+    Student:            StudentsInterface,
+    
+    PetitionID:         number,
+    Petition:           PetitionsInterface,
+    
+    CourseID:           number,
+    Course:             CoursesInterface,
+
+}
