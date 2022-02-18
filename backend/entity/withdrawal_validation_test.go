f81f75c3f7f2a5a70af749f7314c0a1c92ba67fa
narudee2743 <narudee.joy07@gmail.com>
2022-02-18 13:04:38 +0700

    close #243 - add case test validation

--- backend/entity/withdrawal_validation_test.go
+++ backend/entity/withdrawal_validation_test.go
@@ -25,34 +25,50 @@ func TestWithdrawalPass(t *testing.T) {
 
 // ข้อมูลปีการศึกษาผิดพลาด ไม่เป็นตัวเลข 4 หลัก
 func TestYearTimeIsNot4Digit(t *testing.T) {
-	g := NewGomegaWithT(t)
 
-	wd := Withdrawal{
-		YearTime:       20400,
-		RemainCredit:   16,
-		Reason:         "so hard",
-		WithdrawalTime: time.Now(),
+	g := NewGomegaWithT(t)
+	fixtures := []int{
+		-2564,
+		3000,
+		25555,
+		255,
+		0,
 	}
-	ok, err := govalidator.ValidateStruct(wd)
-	g.Expect(ok).ToNot(BeTrue())
-	g.Expect(err).ToNot(BeNil())
-	g.Expect(err.Error()).To(Equal("ข้อมูลปีการศึกษาผิดพลาด"))
+
+	for _, fixture := range fixtures {
+
+		wd := Withdrawal{
+			YearTime:       fixture,
+			RemainCredit:   16,
+			Reason:         "so hard",
+			WithdrawalTime: time.Now(),
+		}
+		ok, err := govalidator.ValidateStruct(wd)
+		g.Expect(ok).ToNot(BeTrue())
+		g.Expect(err).ToNot(BeNil())
+		g.Expect(err.Error()).To(Equal("ข้อมูลปีการศึกษาผิดพลาด"))
+	}
 }
 
 // ข้อมูลหน่วยกิตไม่ถูกต้อง ไม่เป็นจำนวนเต็มบวก
 func TestRemainCreditIsNotPosiviteNumber(t *testing.T) {
 	g := NewGomegaWithT(t)
-
-	wd := Withdrawal{
-		YearTime:       2564,
-		RemainCredit:   -5,
-		Reason:         "so hard",
-		WithdrawalTime: time.Now(),
+	fixtures := []int{
+		-20,
+		0,
 	}
-	ok, err := govalidator.ValidateStruct(wd)
-	g.Expect(ok).ToNot(BeTrue())
-	g.Expect(err).ToNot(BeNil())
-	g.Expect(err.Error()).To(Equal("ข้อมูลหน่วยกิตไม่ถูกต้อง"))
+	for _, fixture := range fixtures {
+		wd := Withdrawal{
+			YearTime:       2564,
+			RemainCredit:   fixture,
+			Reason:         "so hard",
+			WithdrawalTime: time.Now(),
+		}
+		ok, err := govalidator.ValidateStruct(wd)
+		g.Expect(ok).ToNot(BeTrue())
+		g.Expect(err).ToNot(BeNil())
+		g.Expect(err.Error()).To(Equal("ข้อมูลหน่วยกิตไม่ถูกต้อง"))
+	}
 }
 
 // ข้อมูลเหตุผลไม่ถูกต้อง ห้ามเป็นช่องว่าง
@@ -78,15 +94,23 @@ func TestReasonNotBlank(t *testing.T) {
 func TestWithdrawalTimeMustBePresent(t *testing.T) {
 	g := NewGomegaWithT(t)
 
-	wd := Withdrawal{
-		YearTime:       2564,
-		RemainCredit:   16,
-		Reason:         "so hard",
-		WithdrawalTime: time.Now().Add(24 * time.Hour),
+	fixtures := []time.Time{
+		time.Now().Add(24 * time.Hour),
+		time.Now().Add(24 - time.Hour),
 	}
 
-	ok, err := govalidator.ValidateStruct(wd)
-	g.Expect(ok).ToNot(BeTrue())
-	g.Expect(err).ToNot(BeNil())
-	g.Expect(err.Error()).To(Equal("ข้อมูลวันเวลาไม่ถูกต้อง"))
+	for _, fixture := range fixtures {
+
+		wd := Withdrawal{
+			YearTime:       2564,
+			RemainCredit:   16,
+			Reason:         "so hard",
+			WithdrawalTime: fixture,
+		}
+
+		ok, err := govalidator.ValidateStruct(wd)
+		g.Expect(ok).ToNot(BeTrue())
+		g.Expect(err).ToNot(BeNil())
+		g.Expect(err.Error()).To(Equal("ข้อมูลวันเวลาไม่ถูกต้อง"))
+	}
 }
